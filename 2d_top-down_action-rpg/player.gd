3e808ffb8a0b6ba3d52f934dda6e9ec569af7d50
Scott Bergeron <sbergeron28@bfhsla.org>
2025-10-24 15:32:30 -0500

    10/24/25

---
+++ 2d_top-down_action-rpg/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+
+@export var move_speed: float = 150.0
+
+
+func _physics_process(delta: float) -> void:
+	
+	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = move_vector * move_speed
+	if velocity.x > 0:
+		$AnimatedSprite2D.play("move_right")
+	elif velocity.x < 0:
+		$AnimatedSprite2D.play("move_left")
+	elif velocity.y > 0:
+		$AnimatedSprite2D.play("move_down")
+	elif velocity.y < 0:
+		$AnimatedSprite2D.play("move_up")
+	else:
+		$AnimatedSprite2D.stop()
+		
+	move_and_slide()
