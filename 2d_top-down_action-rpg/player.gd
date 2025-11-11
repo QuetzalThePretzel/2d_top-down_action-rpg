efd24f3446b79fe85034224b07690c5646af451f
Scott Bergeron <sbergeron28@bfhsla.org>
2025-11-11 15:37:39 -0600

    11/11/25

--- 2d_top-down_action-rpg/player.gd
+++ 2d_top-down_action-rpg/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name Player
 
 @export var move_speed: float = 150.0
+@export var push_strength: float = 50
 
 func _ready() -> void:
 	position = Scenemanager.player_spawn_position
@@ -20,4 +21,20 @@ func _physics_process(delta: float) -> void:
 	else:
 		$AnimatedSprite2D.stop()
 		
+	var collision: KinematicCollision2D = get_last_slide_collision()
+	if collision:
+		var collider_node = collision.get_collider()
+		if collider_node.is_in_group("Movable"):
+			var collision_normal: Vector2 = collision.get_normal()
+			
+			collider_node.apply_central_force(-collision_normal * push_strength)
 	move_and_slide()
+#Add code to player. 
+#The characterbody doesnt have collision signals
+#(although you could add a rigidbody as a child) 
+#so collision detection needs to be added 
+# - find collision node with get_last_slide_collision() 
+#and get_collider(), if there is a collision with a pushable object, 
+#get direction of collision (normal vector), 
+#apply central force away from player (negative), 
+#deactivate or lock rotation, apply linear damping
