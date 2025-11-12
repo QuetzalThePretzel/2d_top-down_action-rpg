9c484f0693f5b19aca9428b5439337e1b5514346
Scott Bergeron <sbergeron28@bfhsla.org>
2025-11-12 11:54:52 -0600

    11/12/25

--- 2d_top-down_action-rpg/scenemanager.gd
+++ 2d_top-down_action-rpg/scenemanager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var player_spawn_position: Vector2 = Vector2(458, 208)
+var player_spawn_position: Vector2 = Vector2(-280, 72)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
